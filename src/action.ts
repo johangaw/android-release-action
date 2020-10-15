168ca71bb3b422bee03bc0716efa205ad301dc00
Johan Gustavsson <johan.bo.gustavsson@gmail.com>
2020-10-15 14:43:20 +0200

    Use kebak case for action parameters

--- src/action.ts
+++ src/action.ts
@@ -2,11 +2,11 @@ import { getInput, setFailed, setOutput } from "@actions/core";
 import { createRelease } from "./createRelease";
 
 async function main() {
-  const keyFile = getInput("keyFile");
-  const packageName = getInput("packageName");
-  const aabFile = getInput("aabFile");
+  const keyFile = getInput("key-file");
+  const packageName = getInput("package-name");
+  const aabFile = getInput("aab-file");
   const track = getInput("track");
-  const releaseName = getInput("releaseName");
+  const releaseName = getInput("release-name");
 
   const versionCode = await createRelease({
     keyFile,
@@ -16,7 +16,7 @@ async function main() {
     releaseName,
   });
 
-  setOutput("versionCode", versionCode);
+  setOutput("version-code", versionCode);
 }
 
 main().catch(setFailed);
